daaac968a9e14af52f742313ddfc12a75fb2e568
王磊 <11169514@qq.com>
2018-04-25 15:00:53 +0800

    112221121

--- js/index.js
+++ js/index.js
@@ -1,19 +1,6 @@
 document.write("<script language='javascript' src='js/vue.js'></script>");
-document.write("<script language='javascript' src='js/nav.js'></script>");
 
 $(document).ready(function(){
-	nav(data);
-    $(".menu").click(function(){
-    	$('.navListM').slideToggle();
-    });
-    $(".navListM li>a").click(function(){
-    	$(".navListM li .second").hide();
-    	$(this).next(".second").slideToggle();
-    });
-    $('.temp').click(function(){
-    	$('.mask').show();
-    });
-    $('.maskClose').click(function(){
-    	$('.mask').hide();
-    });
+
+
 });
