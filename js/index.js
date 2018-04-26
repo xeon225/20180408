d6fca0556b754dc7f3b08914ecdc6880728bc457
王磊 <11169514@qq.com>
2018-04-26 18:00:55 +0800

    2222

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-document.write("<script language='javascript' src='js/vue.js'></script>");
 
 $(document).ready(function(){
 
 
 });
+var app = new Vue({
+  el: '#app',
+  data: {
+    skill:[1,1,1,1,1,0,0,0],
+    word:[1,1,1,1,1,1,1,0],
+    excel:[1,1,1,1,1,1,1,0]
+  }
+})
